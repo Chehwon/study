5a63fa8646fca5904cd04e4ce41946c631ac3c8c
chehwon <imchehwon@gmail.com>
2024-12-18 18:31:36 +0900

    feat : 버튼 클릭시 메시지 표시 기능 추가

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,17 @@
 const one = document.querySelector(".one");
+const button = document.querySelector(".button");
 
 one.style.background = "blue";
 one.style.color = "white";
 
 one.style.fontSize = "50px";
+
+const click = () => {
+    alert("안녕하세요");
+}
+
+button.addEventListener("click", click);
+
 
 
 const a = 1;
